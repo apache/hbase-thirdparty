a684c3779de27c5db3b8da7909795d8c0e45096a
Nihal Jain <nihaljain@apache.org>
2025-07-19 02:35:17 +0530

    Try Dockerfile change and make release target releated changes

--- dev-support/generate-toolchains.sh
+++ dev-support/generate-toolchains.sh
@@ -41,54 +41,22 @@ if [ -z "$JAVA17_HOME" ]; then
     fi
 fi
 
+TEMPLATE_FILE="./dev-support/toolchains-jenkins.xml"
+OUTPUT_FILE="toolchains.xml"
+
+if [ ! -f "$TEMPLATE_FILE" ]; then
+  echo "Template file $TEMPLATE_FILE not found!"
+  exit 1
+fi
+
 echo "Generating toolchains.xml with:"
 echo "  JAVA8_HOME: $JAVA8_HOME"
 echo "  JAVA17_HOME: $JAVA17_HOME"
 
-# Generate toolchains.xml
-cat > toolchains.xml << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<toolchains xmlns="http://maven.apache.org/TOOLCHAINS/1.1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://maven.apache.org/TOOLCHAINS/1.1.0 http://maven.apache.org/xsd/toolchains-1.1.0.xsd">
-  <!--
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
--->
-  <toolchain>
-    <type>jdk</type>
-    <provides>
-      <version>1.8</version>
-    </provides>
-    <configuration>
-      <jdkHome>$JAVA8_HOME</jdkHome>
-    </configuration>
-  </toolchain>
-  <toolchain>
-    <type>jdk</type>
-    <provides>
-      <version>17</version>
-    </provides>
-    <configuration>
-      <jdkHome>$JAVA17_HOME</jdkHome>
-    </configuration>
-  </toolchain>
-</toolchains>
-EOF
+# Substitute placeholders and write output
+sed \
+  -e "s|/usr/lib/jvm/java-8|$JAVA8_HOME|g" \
+  -e "s|/usr/lib/jvm/java-17|$JAVA17_HOME|g" \
+  "$TEMPLATE_FILE" > "$OUTPUT_FILE"
 
 echo "toolchains.xml generated successfully!"
